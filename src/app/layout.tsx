1e3be48e8fbcb5ccbfcf847c732d287fd3db6b7e
sannhaq <ikhsannhaq@gmail.com>
2025-05-08 13:20:43 +0700

    Ikhsan | fix bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
+    <html lang="en" className="">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
-    </>
+    </html>
   );
 }
